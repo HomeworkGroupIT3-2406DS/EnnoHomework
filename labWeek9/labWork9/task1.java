06bf83f04f7f5954f8a553614210f63035c1fe32
Alikhan-Amandinov <amandinov.alihan@gmail.com>
2025-10-29 15:50:33 +0500

    lab6 and 9

---
+++ labWeek9/labWork9/task1.java
@@ -0,0 +1,13 @@
+class task1 {
+    public static void main(String[] args) {
+        int n = 8;
+        int m = 0;
+
+        try {
+            int ans = n/m;
+            System.out.println("Answer: " + ans);
+        }catch (ArithmeticException e) {
+            System.out.println("Division by 0!");
+        }
+    }
+}
---
+++ labWork6/TestAbstractPet.java
@@ -0,0 +1,7 @@
+package labWork6;
+
+public class TestAbstractPet {
+    public static void main(String[] args) {
+
+    }
+}
